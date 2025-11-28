3b719368ddbd5f01f0bdd36ee15966ec68f9cdf9
LaGarau <lagarauofficial@gmail.com>
2025-11-28 18:07:17 +0545

    removed commented out code

--- app/firebase.js
+++ app/firebase.js
@@ -1,31 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database"; // for Realtime Database
 
-
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAU2Gn7M7mnA7s4t7IUmnNYwaMXPGp48pQ",
-//   authDomain: "realdb-6d53c.firebaseapp.com",
-//   databaseURL: "https://realdb-6d53c-default-rtdb.firebaseio.com",
-//   projectId: "realdb-6d53c",
-//   storageBucket: "realdb-6d53c.firebasestorage.app",
-//   messagingSenderId: "625759197955",
-//   appId: "1:625759197955:web:b9be49b8c9c84dda03a736"
-// };
-
-
-// const app = initializeApp(firebaseConfig);
-// export const db = getDatabase(app); // export the database instance
-
-
-
-
-
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
